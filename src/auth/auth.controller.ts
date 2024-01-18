8cf28224b2ddc36350436a7f05814878da2431c6
sdphat <77732077+sdphat@users.noreply.github.com>
2024-01-18 21:43:40 +0700

    Set sameSite to none

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -42,6 +42,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV !== 'development',
       maxAge: ms(jwtConstants.accessTokenMaxAge),
+      sameSite: 'none',
       domain: url.hostname === 'localhost' ? undefined : url.hostname,
     });
   }
@@ -56,6 +57,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV !== 'development',
       maxAge: ms(jwtConstants.refreshTokenMaxAge),
+      sameSite: 'none',
       domain: url.hostname === 'localhost' ? undefined : url.hostname,
     });
   }
